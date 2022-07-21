6cfc58a00030ee6bc8226b49ac35d0187b11d9ef
Aleksei Petrenko <petrenko@usc.edu>
2022-07-21 03:11:22 -0700

    First PyPI version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # signal-slot
-Qt-like event loops, signals and slots for communication across threads and processes in Python
+
+[![Downloads](https://pepy.tech/badge/signal-slot-mp)](https://pepy.tech/project/signal-slot-mp)
+[<img src="https://img.shields.io/discord/987232982798598164?label=discord">](https://discord.gg/BCfHWaSMkr)
+
+Qt-like event loops, signals and slots for communication across threads and processes in Python.
+
+### Recent releases
+
+##### v1.0.0
+* First PyPI version
+
+### Installation
+
+```bash
+pip install signal-slot-mp
+```
+
+### Contributing
+
+Local installation for development:
+
+```bash
+pip install -e .[dev]
+```
+
+Automatic code formatting:
+
+```bash
+make format && make check-codestyle
+```
+
+Run tests:
+
+```bash
+make test
+```
+
+Documentation contributions are welcome.
+
+### Footnote
+
+Originally designed for Sample Factory 2.0, a high-throughput asynchronous RL codebase https://github.com/alex-petrenko/sample-factory.
+Distributed under MIT License (see LICENSE), feel free to use for any purpose, commercial or not, at your own risk.
+
+See also https://github.com/Numergy/signalslot
---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+    descr_lines = long_description.split("\n")
+    long_description = "\n".join(descr_lines)
+
+
+setup(
+    # Information
+    name="signal-slot-mp",
+    description="Fast and compact framework for communication between threads and processes in Python using event loops, signals and slots.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version="1.0.0",
+    url="https://github.com/alex-petrenko/signal-slot",
+    author="Aleksei Petrenko",
+    license="MIT",
+    keywords="asynchronous multithreading multiprocessing queue faster-fifo signal slot event loop",
+    project_urls={
+        "Github": "https://github.com/alex-petrenko/signal-slot",
+        "Sample Factory": "https://github.com/alex-petrenko/sample-factory",
+    },
+    install_requires=[
+        "faster-fifo>=1.4.2,<2.0",
+    ],
+    extras_require={
+        "dev": ["black", "isort", "pytest<8.0", "flake8", "pre-commit", "twine"],
+    },
+    package_dir={"": "./"},
+    packages=setuptools.find_packages(where="./", include="signal_slot*"),
+    include_package_data=True,
+    python_requires=">=3.7",
+)
